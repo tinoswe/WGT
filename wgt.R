c3314de64f729db8167621e99d30d14352e2f1ab
tinoswe <olivomartino@gmail.com>
2017-06-21 09:21:11 +0200

    ,

--- wgt.R
+++ wgt.R
@@ -35,8 +35,8 @@ vals <- c(110.9,
 plot(dates,
      vals,
      col="black",
-     pch=3,
-     type="n",
+     pch=20,
+     #type="n",
      xaxt="n",
      yaxt="n",
      xaxs="i",
@@ -47,6 +47,14 @@ plot(dates,
      ylim=c(85,115),
      xlim=as.Date(c("2017-01-01",
                     "2017-09-15")))
+
+dates2 <- as.Date(c("2017-06-19",
+                    "2017-06-21"))
+vals2 <- c(104.3,
+           103.6)
+points(dates2,
+       vals2,
+       pch=20)
 
 xdts <- seq(as.Date(c("2017-01-01")),
         as.Date(c("2017-09-15")),
@@ -66,52 +74,53 @@ axis(2,
      las=0.5,
      cex.axis=0.6,
      las=2,
-     col.axis="darkred")
+     col.axis="black")
 
-abline(h=seq(85,
-             115,
-             by=1),
-       col="lightgrey",
-       lty=3)
+# abline(h=seq(85,
+#              115,
+#              by=1),
+#        col="lightgrey",
+#        lty=3)
+# 
+# abline(v=as.Date(c("2017-01-09", "2017-02-09", "2017-03-09", "2017-04-09", "2017-05-09", "2017-06-09", "2017-07-09")),
+#        col="lightgrey",
+#        lty=3)
 
-abline(v=as.Date(c("2017-01-09", "2017-02-09", "2017-03-09", "2017-04-09", "2017-05-09", "2017-06-09", "2017-07-09")),
-       col="lightgrey",
-       lty=3)
+# dts <- as.Date(c("2017-01-09","2017-02-09","2017-03-09","2017-04-09","2017-05-09","2017-06-09","2017-07-09","2017-08-09"),
+#                  "%Y-%m-%d")
+# lbls <- format(dts, format="%d.%m")
+# 
+# points(dts,
+#        c(110.9, 107.4, 103.9, 100.4, 96.9, 93.4, 89.9, 86.4),
+#        pch=25,
+#        cex=1,
+#        col="darkred",
+#        bg="red")
 
-dts <- as.Date(c("2017-01-09","2017-02-09","2017-03-09","2017-04-09","2017-05-09","2017-06-09","2017-07-09","2017-08-09"),
-                 "%Y-%m-%d")
-lbls <- format(dts, format="%d.%m")
+#points(as.Date("2017-02-09",
+#               "%Y-%m-%d"),
+#       106.5,
+#       col="darkred",
+#       pch=19)
 
-points(dts,
-       c(110.9, 107.4, 103.9, 100.4, 96.9, 93.4, 89.9, 86.4),
-       pch=25,
-       cex=1,
-       col="darkred",
-       bg="red")
-points(as.Date("2017-02-09",
-               "%Y-%m-%d"),
-       106.5,
-       col="darkred",
-       pch=19)
+#df <- data.frame(x=dates,
+#                 y=vals)
+#lf <- lm(y ~ x, 
+#         data = df)
+#abline(lf,
+#       lty=2,
+#       col="lightcoral")
+#lines(dates,
+#      vals,
+#      lwd=2,
+#      col="darkred")
 
-df <- data.frame(x=dates,
-                 y=vals)
-lf <- lm(y ~ x, 
-         data = df)
-abline(lf,
-       lty=2,
-       col="lightcoral")
-lines(dates,
-      vals,
-      lwd=2,
-      col="darkred")
-
-BMI = function(height_m,weight_kg){(1.3*weight_kg/(height_m)^2.5)}
-h = 1.85
-w = vals
-my_bmi = data.frame(h,w)
-my_bmi$bmi = BMI(my_bmi$h,
-                 my_bmi$w)
+#BMI = function(height_m,weight_kg){(1.3*weight_kg/(height_m)^2.5)}
+#h = 1.85
+#w = vals
+#my_bmi = data.frame(h,w)
+#my_bmi$bmi = BMI(my_bmi$h,
+#                 my_bmi$w)
 
 # par(new = TRUE)
 # plot(dates,
